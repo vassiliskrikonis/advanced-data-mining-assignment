6da7e5ad3ddfc0e78404e0e0dd2b24be72697236
Vassilis Krikonis <vasiliskrikonis@gmail.com>
2019-02-26 21:48:51 +0200

    Recognise low speed simple event

--- flinkcep/src/main/scala/com/vassiliskrikonis/Events.scala
+++ flinkcep/src/main/scala/com/vassiliskrikonis/Events.scala
@@ -3,8 +3,18 @@ package com.vassiliskrikonis
 import scala.util.Try
 
 object Events {
-  case class AIS(mmsi: String, status: Int, speed: Float, lon: Float, lat: Float, ts: Long)
-  case class extendedAIS(mmsi: String, status: Int, speed: Float, lon: Float, lat: Float, portDist: Double, ts: Long, delta: Long)
+  type Mmsi = String
+
+  object Thresholds {
+    val MIN_GAP_DURATION: Int = 5 * 60 // in seconds
+    val FAR_FROM_PORT: Double = 5000 // in meters
+    val LOW_SPEED: Float = 1 // in knots
+  }
+
+  case class AIS(mmsi: Mmsi, status: Int, speed: Float, lon: Float, lat: Float, ts: Long)
+  case class extendedAIS(mmsi: Mmsi, status: Int, speed: Float, lon: Float, lat: Float, portDist: Double, ts: Long, delta: Long)
+  case class LowSpeed(mmsi: Mmsi, speed: Float)
+  case class Stopped(mmsi: Mmsi, portDist: Double)
 
   def parseSignal(s: String): Try[AIS] = {
     // "id","mmsi","status","turn","speed","course","heading","lon","lat","ts","geom"
@@ -14,6 +24,5 @@ object Events {
     Try(AIS(mmsi, status.toInt, speed.toFloat, lon.toFloat, lat.toFloat, ts.toLong))
   }
 
-  val MIN_GAP_DURATION: Int = 5 * 60 // in seconds
-  val vesselFarFromPort: extendedAIS => Boolean = (s: extendedAIS) => s.portDist > 5000
+  val vesselFarFromPort: extendedAIS => Boolean = (s: extendedAIS) => s.portDist > Thresholds.FAR_FROM_PORT
 }
